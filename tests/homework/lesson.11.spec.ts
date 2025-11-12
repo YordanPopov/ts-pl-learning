1d82d5925cae060681e19e84d083b7aa5b4eeadd
YordanPopov <jordanpopov190@gmail.com>
2025-11-12 21:04:17 +0200

    add lesson 11 homework

--- tests/homework/lesson.11.spec.ts
+++ tests/homework/lesson.11.spec.ts
@@ -1,18 +1,14 @@
-import { test, expect } from '@playwright/test';
 // use the code gen tool in VScode Testing tab to generate the following scenario:
+// go to https://pragmatic.bg/
+// hover on "Курсове"
+// click on "Автоматизирано Тестване с Playwright и TypeScript"
+// check the name of lecture 12
+// afterwords you will have a new file in project/tests folder. Copy its contents into this file and remove the old one.
+import { test, expect } from '@playwright/test';
 
-test('Verify the Name of Lecture 12', { tag: '@homework' }, async ({ page }) => {
-  // go to https://pragmatic.bg/
+test('test', async ({ page }) => {
   await page.goto('https://pragmatic.bg/');
-
-  // hover on "Курсове"
   await page.getByRole('link', { name: 'Курсове', exact: true }).hover();
-
-  // click on "Автоматизирано Тестване с Playwright и TypeScript"
-  await page.locator('#menu-item-9220').getByRole('link', { name: 'Автоматизирано Тестване с Playwright и TypeScript' }).click();
-
-  // check the name of lecture 12
+  await page.locator('#menu-item-9220').getByRole('link', { name: 'Автоматизирано Тестване с' }).click();
   await expect(page.locator('ol')).toContainText('Основи на тестването с Playwright');
 });
-
-// afterwords you will have a new file in project/tests folder. Copy its contents into this file and remove the old one.
